d8ccb518f3ed270fa633bfd9a3ab9b62ce6b0ce6
Navneet-Patnecha <navneetchippa@gmail.com>
2023-10-01 18:16:40 +0530

    first commit

---
+++ src/com/navneet/Linked_List/Main.java
@@ -0,0 +1,19 @@
+package com.navneet.Linked_List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Sigly_linked_list list = new Sigly_linked_list();
+        list.insertFirst(3);
+        list.insertFirst(5);
+        list.insertFirst(33);
+        list.insertFirst(5);
+        list.insertLast(90 );
+        list.insertFirst(65);
+
+        list.display();
+
+    }
+
+
+}
